52cc83b0b7e9087c340a3739a1f7a66fd0ab64ce
sood-radhika <50200122+sood-radhika@users.noreply.github.com>
2019-10-12 18:18:45 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
-public class Hello{
-		public static void main(String[] arg)
-				{
-						system.out.print("Hello World");
-								}
-}
+class Hello 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello World"); 
+    } 
+} 
